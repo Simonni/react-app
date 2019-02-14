25ec90ac57e5016ec57592b428d06e62fb18f7bc
Simonni <szkiflay@gmal.com>
2019-02-14 10:02:47 -0500

    additing delete

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
  state ={
-      value: this.props.value,
+      value: this.props.counter.value,
       
  };
 
     handleIncrement=()=>{
       this.setState({value: this.state.value + 1})
  }
+
   render() { 
 
     return (
@@ -18,6 +19,9 @@ class Counter extends Component {
       <button 
       onClick={this.handleIncrement}
       className='btn btn-secondary btn-sm'>Increment
+      </button>
+      <button onClick={()=>this.props.onDelete(this.props.counter.id)} 
+      className='btn btn-danger btn-sm m-2'>Delete
       </button>
       </div> 
     )
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -8,11 +8,19 @@ class Counters extends Component {
     {id: 3, value: 0},
     {id: 4, value: 0},
   ]  }
+
+  handleDelete=counterId=>{
+    const counters= this.state.counters.filter(c=>c.id !==counterId);
+    this.setState({counters})
+  }
+
   render() { 
     return ( <div>
-      <h1>This is Simon</h1>
       {this.state.counters.map(counter=> 
-      <Counter key={counter.id} value={counter.value}/>)}
+      <Counter key={counter.id} 
+      onDelete={this.handleDelete} 
+      counter={counter}
+      />)}
     </div> );
   }
 }
