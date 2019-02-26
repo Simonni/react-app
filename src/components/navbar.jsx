c2567233be661e808471091f27e1e4d2c70b5171
Simonni <szkiflay@gmal.com>
2019-02-26 09:38:33 -0500

    stateless components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-
-  render() { 
+const NavBar = (props) => { 
     return ( <div className="container">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="#">
+      Navbar <span className="badge badge-pill badge-secondary">
+      {props.totalCounters}
+      </span>
+      </a>
     </nav>
-  </div> );
-  }
-}
+  </div> 
+  );
+  };
  
 export default NavBar;
