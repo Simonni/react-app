26c78bbc24b05ecd5b22662ea9b25ab712e6bd55
Simonni <szkiflay@gmal.com>
2019-03-07 15:05:29 -0500

    deleted

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,7 +4,7 @@ import Counter from './counter'
 class Counters extends Component {
  
   render() { 
-    const {onReset, onDelete, onIncrement, counters, onDecrement} = this.props
+    const {onReset, onDelete, onIncrement, counters} = this.props
     return ( <div>
       <button onClick= {onReset} className="btn btn-primary btn-sm m-2">Reset
       </button>
